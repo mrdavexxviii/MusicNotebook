b08584921b173625ed85c539532a13ac86b70860
Dave <dave@anemail.gfhfghf>
2026-03-05 20:03:50 +0000

    Set application size

---
+++ MusicNotebook/ScreenSizeProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MusicNotebook
+{
+
+    public class ScreenSizeProvider 
+    {
+        double verticalRatio = 0.8;
+        double horizontalRatio = 0.8;
+        double taskbarOffset = 30;
+        public double AppStartWidth => SystemParameters.PrimaryScreenWidth * horizontalRatio;
+        public double AppStartHeight => SystemParameters.PrimaryScreenHeight * verticalRatio;
+        public double HorizontalMargin => SystemParameters.PrimaryScreenWidth * (1-horizontalRatio)/2;
+        public double VerticalMargin => Math.Max (0, (SystemParameters.PrimaryScreenHeight * (1 - verticalRatio) / 2) - taskbarOffset);
+    }
+}
